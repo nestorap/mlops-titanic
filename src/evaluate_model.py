16065a3eb0c3d80e7d9705a0d455dd4f10ee716f
nestorap <nalaminospelegrina@gmail.com>
2023-05-23 18:13:10 +0200

    subiendo integración con mlflow. Me falta la función helper

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,11 +1,14 @@
 '''
-En este scrip tenemos el código que nos saca las predicciones del modelo
+En este scrip tenemos el código que nos saca las predicciones del modelo.
+También incluimos integración con MLflow para versionar el modelo
 '''
 # Librerías
 
 import pandas as pd
 import numpy as np
+import mlflow
 import preprocessors as pp
+from helper import BaseLogger
 
 from pipeline import pipeline
 import hydra
@@ -13,6 +16,7 @@ from hydra.utils import to_absolute_path as abspath
 import joblib # Para guardar el modelo
 import os
 from omegaconf import (DictConfig, OmegaConf)
+
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
@@ -22,6 +26,7 @@ from sklearn.metrics import (confusion_matrix,
                              f1_score)
 from sklearn.model_selection import train_test_split
 
+logger = BaseLogger()
 
 def load_data(path: DictConfig):
     '''
@@ -42,26 +47,51 @@ def load_model(model_path: str):
 def predict(model: LogisticRegression, X_test: pd.DataFrame):
     return model.predict(X_test)
 
+def log_params(model: LogisticRegression, features: list):
+    logger.log_params({"model_class": type(model).__name__})
+    model_params = model.get_params()
+
+    for ag, value in model_params_items():
+        logger.log_params({arg: value})
+    logger.log_params({"features": features})
+
+def log_metrics(**metrics:dict):
+    logger.log_metrics(metrics)
+
 # Función en la que ejecutamos las funciones realizadas arriba
 @hydra.main(version_base=None, config_path="../config/", config_name="config")
 def evaluate(config: DictConfig):
-    # Cargamos la data
-    X_test, y_test = load_data(config.processed)
+    mlflow.set_tracking_uri(config.mlflow_tracking_ui)
+    mlflow.set_experiment("primer_experimento")
 
-    # Cargamos el modelo
-    model = load_model(abspath(config.model.path))
+    with mlflow.start_run():
+        # Cargamos la data
+        X_test, y_test = load_data(config.processed)
 
-    # Obtenemos las predicciones
-    yhat = predict(model, X_test)
 
-    # Obtenemos métricas
-    f1 = f1_score(y_test, yhat)
-    print("###################")
-    print(f'El f1 score es de --> {f1}')
+        # Cargamos el modelo
+        model = load_model(abspath(config.model.path))
 
-    acc = accuracy_score(y_test, yhat)
-    print("###################")
-    print(f'El accuracy es de --> {acc}')
+        # Obtenemos las predicciones
+        yhat = predict(model, X_test)
+
+        # Obtenemos métricas
+        f1 = f1_score(y_test, yhat)
+        print("###################")
+        print(f'El f1 score es de --> {f1}')
+
+        acc = accuracy_score(y_test, yhat)
+        print("###################")
+        print(f'El accuracy es de --> {acc}')
+
+        # Log metrics en mlflow
+        log_params(model, config.process.features)
+        log_metrics(f1_score=f1, accuracy_score=acc)
+    
+        # Aquí tenemos el código para que mlflow nos lo guarde en local
+        mlflow.sklearn.log_model(model, "model")
+        mlflow.log_metric("f1_score", f1)
+        mlflow.log_metric("accuracy", acc)
 
 if __name__ == "__main__":
     evaluate()
--- src/tests/test_train_model.py
+++ src/tests/test_train_model.py
@@ -13,4 +13,27 @@ from hydra.utils import to_absolute_path as abspath
 from src.train_model import load_data
 
 def test_reglog():
+    '''
+    En esta función comprobamos que el modelo funciona bien. Los pasos de esta función son:
+    1 - Cargamos el modelo ayudándonos de hydra y joblib.
+    2- Cargamos los dataset de train y test y concatenamos con las variable target.
+    3- Hacemos el check de que el modelo funciona bien y le ponemos un mínimo que tiene que cumplir.
+    '''
+
+    with initialize(version_base=None, config_path="../../config"):
+        config = compose(config_name="main")
+
+    model_path = abspath(config.model.path)
+    model = joblib.load(model_path)
+
+    X_train, X_test, y_train, y_test = load_data(config.processed)
+
+    train_df = pd.concat([X_train, y_train], axis=1)
+    test_df = pd.concat([X_test, y_test], axis=1)
+
+    train_ds = Dataset(train_df, label="survived")
+    validation_ds = Dataset(test_df, label="survived")
+
+    check = ModelErrorAnalysis(min_error_model_score=0.3)
+    check.run(train_ds, validation_ds, model)
 
